76f78a2e121d2fdcbdd0b161c71b956c4883c697
Marco-Schneider <marco.schneider452@gmail.com>
2023-12-10 13:47:55 -0300

    Creating header file to store conveyor pins

---
+++ lib/Esteiras/Esteiras.h
@@ -0,0 +1,30 @@
+#ifndef _Esteiras_h_
+#define _Esteiras_h_
+
+const int passosPorRevolucao = 2048;
+
+//Esteira 1
+const int esteira1_IN1 = 33;
+const int esteira1_IN2 = 32;
+const int esteira1_IN3 = 21;
+const int esteira1_IN4 = 22;
+
+//Esteira 2
+const int esteira2_IN1 = 18;
+const int esteira2_IN2 = 19;
+const int esteira2_IN3 = 21;
+const int esteira2_IN4 = 22;
+
+//Esteira 3
+const int esteira3_IN1 = 5;
+const int esteira3_IN2 = 17;
+const int esteira3_IN3 = 16;
+const int esteira3_IN4 = 4;
+
+//Esteira 4
+const int esteira4_IN1 = 14;
+const int esteira4_IN2 = 27;
+const int esteira4_IN3 = 26;
+const int esteira4_IN4 = 25;
+
+#endif
